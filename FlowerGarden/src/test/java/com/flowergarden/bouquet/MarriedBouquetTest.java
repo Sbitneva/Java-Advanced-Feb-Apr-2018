6953e22f3a657076f3646e3f8970ab43dfee1890
Sbitneva <masha.sbitneva@gmail.com>
2018-03-03 14:12:19 +0200

    test: MarriedBouquet unit-tests

---
+++ FlowerGarden/src/test/java/com/flowergarden/bouquet/MarriedBouquetTest.java
@@ -0,0 +1,90 @@
+package com.flowergarden.bouquet;
+
+import com.flowergarden.flowers.Chamomile;
+import com.flowergarden.flowers.GeneralFlower;
+import com.flowergarden.flowers.Rose;
+import com.flowergarden.flowers.Tulip;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Collection;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class MarriedBouquetTest {
+
+    private MarriedBouquet marriedBouquet;
+    private Chamomile chamomile = mock(Chamomile.class);
+    private Rose rose = mock(Rose.class);
+    private Tulip tulip = mock(Tulip.class);
+
+    @Before
+    public void createMarriedBouquet(){
+
+        marriedBouquet = new MarriedBouquet();
+        marriedBouquet.addFlower(chamomile);
+        marriedBouquet.addFlower(rose);
+        marriedBouquet.addFlower(tulip);
+    }
+
+    @Test
+    public void chamomileShouldBeFoundedByLength(){
+
+        when(chamomile.getLength()).thenReturn(20);
+        when(rose.getLength()).thenReturn(150);
+        when(tulip.getLength()).thenReturn(99);
+
+        Collection<GeneralFlower> generalFlowerList = marriedBouquet.searchFlowersByLength(15, 40);
+        assertEquals(generalFlowerList.size(), 1);
+        assertTrue(generalFlowerList.contains(chamomile));
+
+    }
+
+    @Test
+    public void chamomileAndTulipShouldBeFoundedByLength(){
+
+        when(chamomile.getLength()).thenReturn(20);
+        when(rose.getLength()).thenReturn(150);
+        when(tulip.getLength()).thenReturn(99);
+
+        Collection<GeneralFlower> generalFlowerList = marriedBouquet.searchFlowersByLength(15, 99);
+        assertEquals(generalFlowerList.size(), 2);
+        assertTrue(generalFlowerList.contains(chamomile));
+        assertTrue(generalFlowerList.contains(tulip));
+    }
+
+    @Test
+    public void bouquetPriceShouldBe720(){
+
+        when(chamomile.getPrice()).thenReturn(100f);
+        when(rose.getPrice()).thenReturn(200f);
+        when(tulip.getPrice()).thenReturn(300f);
+
+        float price = 0;
+        for(GeneralFlower flower : marriedBouquet.getFlowers()) {
+            price += flower.getPrice();
+        }
+        price += marriedBouquet.getAssemblePrice();
+
+        assertTrue(marriedBouquet.getPrice() == price);
+    }
+
+    @Test
+    public void bouquetPriceShouldBe(){
+
+        when(chamomile.getPrice()).thenReturn(100f);
+        when(rose.getPrice()).thenReturn(200f);
+        when(tulip.getPrice()).thenReturn(-100f);
+
+        float price = 0;
+        for(GeneralFlower flower : marriedBouquet.getFlowers()) {
+            price += flower.getPrice();
+        }
+        price += marriedBouquet.getAssemblePrice();
+
+        assertTrue(marriedBouquet.getPrice() == price);
+    }
+}
