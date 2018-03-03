1c04fb71fb10974488b95fe990e5515133f99f60
Sbitneva <masha.sbitneva@gmail.com>
2018-03-03 13:12:51 +0200

    reformat: style fixes and typo issues

--- FlowerGarden/src/main/java/com/flowergarden/bouquet/Bouquet.java
+++ FlowerGarden/src/main/java/com/flowergarden/bouquet/Bouquet.java
@@ -1,10 +1,15 @@
 package com.flowergarden.bouquet;
+
 import java.util.Collection;
 
 public interface Bouquet<T> {
-	float getPrice();
-	void addFlower(T flower);
-	Collection<T> searchFlowersByLenght(int start, int end);
-	void sortByFreshness();
-	Collection<T> getFlowers();
+    float getPrice();
+
+    void addFlower(T flower);
+
+    Collection<T> searchFlowersByLength(int start, int end);
+
+    void sortByFreshness();
+
+    Collection<T> getFlowers();
 }
